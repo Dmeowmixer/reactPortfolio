c4f5abe72fb6cd3f125cfc9b82f356151d90e5a0
Dmeowmixer <ransonikuo@gmail.com>
2018-09-28 23:55:54 -1000

    removing under construction text

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,6 @@ const IndexPage = () => (
     <p>Welcome to my personal portfolio. This page is still currently under construction!</p>
     <p>Here you will find some information about me and a few of the projects I am working on.</p>
     <p>This site was created using Gatsby JS, React and Reactstrap (Bootstrap 4)</p>
-    <h2>THIS SITE IS UNDER CONSTRUCTION</h2>
   </Layout>
 )
 

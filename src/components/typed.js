9879485c07561cac06ca15580b0099e6e566fe16
Dmeowmixer <ransonikuo@gmail.com>
2019-03-27 19:50:38 -1000

    adding svg developer badges to represent experience

---
+++ src/components/typed.js
@@ -0,0 +1,15 @@
+// import React,{ Component } from 'react';
+// import { render } from 'react-dom';
+// import Typed from 'react-typed';
+
+// class TypedJS extends Component {
+//   render() {
+//     return (<Typed
+//       typedRef={(typed) => { this.typed = typed; }}
+//       strings={['Greetings', 'I am Ranson', 'I am a fullstack Javascript Developer', 'I am an avid learner']} 
+//       typeSpeed={40}/>
+//     );
+//   }
+// }
+
+// export default TypedJS

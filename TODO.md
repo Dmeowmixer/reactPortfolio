72d7e3950e1abba4457b26fcdbc114aa990f396e
Dmeowmixer <ransonikuo@gmail.com>
2019-03-27 14:16:41 -1000

    edits home text and todo.md

--- TODO.md
+++ TODO.md
@@ -1,4 +1,6 @@
-Change theme to dark (similar to darkreader colors)
-Change status bars to badges
-Enlarge navbar
-Add more asthetic packages
+##Change theme to dark (similar to darkreader colors)
+##Change status bars to badges
+##Change My Resume text to clickable screencap img of resume
+##Enlarge navbar
+##Add more asthetic packages
+  * Typed.js intro
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,17 +9,11 @@ import Layout from '../components/layout';
 export default () => (
   <Layout>
     <div>
-      <h1 className="justifyCenter"> My name is Ranson </h1>
+      <h1 className="justifyCenter">Hello, i'm Ranson </h1>
       <img src="https://res.cloudinary.com/drgess24j/image/upload/v1539769697/yellingRanson.jpg"/>
       <h2 className="justifyCenter">I am a fullstack Javascript developer from Honolulu, Hawaii</h2>
-      <p>I am primarily looking for remote work however, I am open to any and all opportunities. I started developing about 5 years ago since I have always been attracted towards technology and constantly learning.
-      Learning to program has been one of the most challenging things of my life, and has taught me many valuable lessons such as overcoming failure. I have always been able to pick things up with relative ease and overcoming the constant feelings of failure programmers feel has helped me grow as an individual.
-      I have worked within the tech field for the past 5 years in varying roles from Independant contractor, freelancer, and a CTO role of a startup. 
-      I had some time to re-examine my goals and have chosen to start working on my development skills once more.</p>
-      <h2 className="justifyCenter">Hobbies</h2>
-      <p>In my spare time I am an avid martial artist and hold ranks in several different styles including Wing Chun, Brazillian Jujitsu, Danzan Ryu Jujitsu, Jeet Kune Do, and Muay Thai to name a few.
-      I also enjoy working on cars and am currently restoring a 1987 Chevrolet Monte Carlo SS. On the programming side of things, I have been learning indie game development in GameMakerStudio2 using GameMakerLanguage which is a Javascriptesque language.
-      Lastly I enjoy tinkering with Raspberry Pis and have worked on WPA/WPA2 WiFi cracking using aircrack-ng and packet sniffing with ncap. I also hope to get a robotics kit to start utilizing the Johnny Five robotics library.</p>
+      <p>I started programming in JS about 5 years ago, and have been constantly learning and trying new things.
+      Learning to program has been one of the most challenging things of my life, and has taught me many valuable lessons such as overcoming failure. I have always been able to pick things up with relative ease and overcoming the constant feelings of failure programmers feel has helped me grow as an individual.</p>
       <h2 className="justifyCenter"><a href={pdf} target="_blank">My Resume</a></h2>      
       <h2 className="justifyCenter">Main Programming Languages</h2>
         <div className="text-center progressBar">HTML</div>

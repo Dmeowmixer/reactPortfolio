22fca971fa5bfe754a3bce8596b4470f74682ebd
Dmeowmixer <ransonikuo@gmail.com>
2019-03-29 12:06:39 -1000

    alters project cards to take full width

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { Link } from 'gatsby';
-import { Card, CardImg, CardTitle, CardText, CardGroup, CardLink, CardBody } from 'reactstrap';
+import { Container, Col, Row, Card, CardImg, CardTitle, CardText, CardGroup, CardLink, CardBody } from 'reactstrap';
 import Layout from '../components/layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import ProjectCard from '../components/projectCards';
@@ -9,54 +9,70 @@ import pdf from '../images/resume.pdf';
 export default () => (
   <Layout>
     <h1 className="justifyCenter">Projects</h1>
-    <CardGroup>
-      <Card className="projectCard">
-        <a href="https://dmeowsmartbrain.herokuapp.com/"> <CardImg  href="https://dmeowsmartbrain.herokuapp.com/" top width="100%" src="https://res.cloudinary.com/drgess24j/image/upload/v1539768252/facialRecognition-min.png" alt="facialRecognition" /> </a>
-        <CardBody>
-          <CardTitle>Facial Image Recgonition</CardTitle>
-          <CardText>
-            <ul>
-              <li>Clarifai API Integration</li>
-              <li>NodeJS Server Hosted on Heroku</li>
-              <li>Express API</li>
-              <li>PostgreSQL Hosted on Heroku</li>
-              <li>React frontend with state management</li>
-              <li>Full user Authentication and registration</li>
-            </ul>
-          </CardText>
-          <CardLink href="https://dmeowsmartbrain.herokuapp.com/">Project Hosted on Heroku</CardLink>
-        </CardBody>
-      </Card>
-      <Card className="projectCard">
-        <a href="https://www.crosscurrenthi.org/"><CardImg top width="100%" src="https://res.cloudinary.com/drgess24j/image/upload/v1539768039/crossCurrent-min.png" alt="CrossCurrentFellowship" /></a>
-        <CardBody>
-          <CardTitle>CrossCurrentFellowship Hawaii</CardTitle>
-          <CardText>
-            <ul>
-              <li>Single Page App</li>
-              <li>JAMstack</li>
-              <li>MD Bootstrap 4</li>
-              <li>Hosted on Netlify</li>
-              <li>Built with Widgets and Apis</li>
-            </ul>
-          </CardText>
-          <CardLink href="https://www.crosscurrenthi.org/">Website</CardLink>
-        </CardBody>
-      </Card>
-      <Card className="projectCard">
-        <a href="https://dmeowmixer.github.io/simpleReact/"><CardImg top width="100%" src="https://res.cloudinary.com/drgess24j/image/upload/v1539768039/simpleReact-min.png" alt="reactApp" /></a>
-        <CardBody>
-          <CardTitle>React/Redux Async API call App</CardTitle>
-          <CardText><ul>
-            <li>React/Redux</li>
-            <li>Fetch API</li>
-            <li>Asynchrounous GET Requests</li>
-            <li>Real Time Query Responses</li>
-          </ul></CardText>
-          <CardLink href="https://dmeowmixer.github.io/simpleReact/">Project Site on GH Pages</CardLink>
-        </CardBody>
-      </Card>
-    </CardGroup>
+    <Container>
+      <CardGroup>
+       <Row>
+          <Col>
+            <Card className="projectCard">
+              <a href="https://dmeowsmartbrain.herokuapp.com/"> <CardImg  href="https://dmeowsmartbrain.herokuapp.com/" top width="100%" src="https://res.cloudinary.com/drgess24j/image/upload/v1539768252/facialRecognition-min.png" alt="facialRecognition" /> </a>
+              <CardBody>
+                <CardTitle>Facial Image Recgonition</CardTitle>
+                <CardText>
+                  <ul>
+                    <li>Clarifai API Integration</li>
+                    <li>NodeJS Server Hosted on Heroku</li>
+                    <li>Express API</li>
+                    <li>PostgreSQL Hosted on Heroku</li>
+                    <li>React frontend with state management</li>
+                    <li>Full user Authentication and registration</li>
+                  </ul>
+                </CardText>
+                <CardLink href="https://dmeowsmartbrain.herokuapp.com/">Project Hosted on Heroku</CardLink>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Card className="projectCard">
+              <a href="https://www.crosscurrenthi.org/"><CardImg top width="100%" src="https://res.cloudinary.com/drgess24j/image/upload/v1539768039/crossCurrent-min.png" alt="CrossCurrentFellowship" /></a>
+              <CardBody>
+                <CardTitle>CrossCurrentFellowship Hawaii</CardTitle>
+                <CardText>
+                  <ul>
+                    <li>Single Page App</li>
+                    <li>JAMstack</li>
+                    <li>MD Bootstrap 4</li>
+                    <li>Hosted on Netlify</li>
+                    <li>Built with Widgets and Apis</li>
+                  </ul>
+                </CardText>
+                <CardLink href="https://www.crosscurrenthi.org/">Website</CardLink>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Card className="projectCard">
+              <a href="https://dmeowmixer.github.io/simpleReact/"><CardImg top width="100%" src="https://res.cloudinary.com/drgess24j/image/upload/v1539768039/simpleReact-min.png" alt="reactApp" /></a>
+              <CardBody>
+                <CardTitle>React/Redux Async API call App</CardTitle>
+                <CardText><ul>
+                  <li>React/Redux</li>
+                  <li>Fetch API</li>
+                  <li>Asynchrounous GET Requests</li>
+                  <li>Real Time Query Responses</li>
+                </ul></CardText>
+                <CardLink href="https://dmeowmixer.github.io/simpleReact/">Project Site on GH Pages</CardLink>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </CardGroup>
+     </Container>
     <div>
       <ul>
         <li><h3>Listed Below are a few more projects, feel free to browse my<a href="https://github.com/Dmeowmixer"> GitHub </a>for a complete list of public repositories I can share.</h3></li>

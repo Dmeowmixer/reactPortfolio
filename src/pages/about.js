0893181747521d9215613507e039c359c10887af
Dmeowmixer <ransonikuo@gmail.com>
2019-03-29 14:48:40 -1000

    adds about me section in about me

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,6 +9,9 @@ import Layout from '../components/layout';
 export default () => (
   <Layout>
     <div>
+      <h2 className="justifyCenter">Who I am</h2>
+      <p>Born and raised in Hawaii, I have always had an insatiable curiousity and a hunger to learn. I can be found reading self-help, science fiction (Roberit Heinlein!), or martial arts books.
+      I try to help others at every opportunity, and would love to impact lives with my work.</p>
       <h2 className="justifyCenter">Hobbies</h2>
       <p>In my spare time I am an avid martial artist and hold ranks in several different styles including Wing Chun, Brazillian Jujitsu, Danzan Ryu Jujitsu, Jeet Kune Do, and Muay Thai to name a few.
       I also enjoy working on cars and am currently restoring a 1987 Chevrolet Monte Carlo SS. On the programming side of things, I have been learning indie game development in GameMakerStudio2 using GameMakerLanguage which is a Javascriptesque language and Unity for VR games.
